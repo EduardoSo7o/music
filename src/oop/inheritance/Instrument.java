c199cd4f9a1ed6ad747a1a25899149381f5b1a63
Luis Eduardo Carreón Soto <60121944+EduardoSo7o@users.noreply.github.com>
2020-03-12 11:57:45 -0600

    Se completaron las clases e interfaces

---
+++ src/oop/inheritance/Instrument.java
@@ -0,0 +1,12 @@
+package oop.inheritance;
+
+
+public interface Instrument {
+
+    void play();
+
+    String what();
+
+    void adjust();
+
+}
---
+++ src/oop/inheritance/Woodwind.java
@@ -0,0 +1,17 @@
+package oop.inheritance;
+
+
+public class Woodwind extends Wind {
+    public Woodwind() {
+        super();
+    }
+
+    public void play() {
+    }
+
+    public String what() {
+        return null;
+    }
+
+}
+
